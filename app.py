63e04b97ba2184568c265028471ddf3736707428
Malvino <149816852+malvino11-28@users.noreply.github.com>
2026-04-03 10:46:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def get_db_connection():
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
+        port=os.getenv("DB_PORT")
     )
 
 @app.route('/getTables', methods = ['GET'])
